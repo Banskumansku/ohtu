64769cb21c9ccf8c70566fcea9e379276c168fbf
Banskumansku <jan.lindholm@helsinki.fi>
2019-11-11 18:35:15 +0200

    Unbreaking stuff

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,12 +5,5 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
-        if (1==1) {
-            if (1==1) {
-                if (1==1) {
-                    System.out.println("asdasd");
-                }
-            }
-        }
     }
 }
