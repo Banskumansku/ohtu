5fdc52c0dd2757a4a0d03bab8ee55f9594bd7be3
Banskumansku <jan.lindholm@helsinki.fi>
2019-11-11 18:30:43 +0200

    trying to break a thing

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,5 +5,12 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
+        if (1==1) {
+            if (1==1) {
+                if (1==1) {
+                    System.out.println("asdasd");
+                }
+            }
+        }
     }
 }
